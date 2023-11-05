01b977fc5f495419e0cbf93c24f963d3a0aa87cb
leolint <maiacalscream@gmail.com>
2023-11-05 13:33:53 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function openQuestionDescription () {
+  const buttons = document.querySelectorAll('.questions_wrapper-item--title');
+
+  buttons.forEach(button => {
+    const arrow = button.children[1]
+    const text = button.nextElementSibling
+
+    button.addEventListener('click' , function() {
+        text.classList.toggle('openDescriptionText')
+        arrow.classList.toggle('arrowRotate')
+    })
+
+  })
+
+}
+
+openQuestionDescription();
+
+function openMenu () {
+  const menu = document.querySelector('.header_mobileNav')
+  const button = document.querySelector('.header_mobileButton')
+
+  button.addEventListener('click' , function () {
+    menu.classList.toggle('openMenu')
+  })
+}
+
+openMenu()
+
---
+++ js/swiper.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  direction: 'horizontal',
+  loop: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+console.log(innerWidth);
+
+if (window.innerWidth < 1100) {
+  swiper.params.slidesPerView = 2;
+  swiper.update();
+}
+if (window.innerWidth < 500){
+  swiper.params.slidesPerView = 1;
+  swiper.update();
+}
+
+
+
+
